c52b9b3c85d9666d061a8e7188e2e0940688189f
BlackPaladin2022 <99116758+BlackPaladin2022@users.noreply.github.com>
2026-02-07 22:40:51 -0500

    moving script dumps
    Moving script dumps of games that have existing translation patches to a Retired Script Dumps

--- (NES) Reigen Doushi MIX/doushi-textdump-RAW.bat
+++
@@ -1,7 +0,0 @@
-cartographer doushi.nes doushi-RAW_commands.txt doushi-RAW_script -m
-
-pause
-
-cartographer doushi.nes doushi-RAW2_commands.txt doushi-RAW2_script -m
-
-pause
---
+++ Retired Text Dumps/(NES) Reigen Doushi MIX/doushi-textdump.bat
@@ -0,0 +1,3 @@
+cartographer doushi.nes doushi2_commands.txt doushi_script -m
+
+pause
